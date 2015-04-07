705966a1de67df31309eecb70221966948cd2074
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-04-07 09:41:53 -0400

    deploy to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 // Database URL
 module.exports = {
     // Uncomment to connect with MongoDB on Cloud
-    // 'url' : 'mongodb://username:password@kahana.mongohq.com:10073/nodeapi'
-    'url' : 'mongodb://admin:admin@ds053251.mongolab.com:53251/todoapp'
+    // 'url' : 'mongodb://admin:admin@ds053251.mongolab.com:53251/todoapp'
+    'url' : 'mongodb://admin:admin@ds061671.mongolab.com:61671/heroku_app35644233'
 };
