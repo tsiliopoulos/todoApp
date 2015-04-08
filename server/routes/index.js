1786abb85bd9cd55f719d06cb9a55e6d0a3c3eaf
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-04-08 07:11:28 -0400

    updated index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -102,7 +102,8 @@ router.get('/auth/google', passport.authenticate('google',{
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email'
-    ]
+    ],
+    successRedirect : '/'
 }),
     function(req, res){
 });
